cc679b0d3e42196eb3f5c2fb325f065a7f249806
Richard Valliant <rvallian@umd.edu>
2017-11-23 22:20:04 +0000

    version 0.7

--- R/wtdvar.R
+++ R/wtdvar.R
@@ -1,5 +1,9 @@
-
-wtdvar <- function(x, w){
+wtdvar <- function(x, w, na.rm = TRUE){
+    if (na.rm){
+        isNA <- is.na(x) | is.na(w)
+        x <- x[!isNA]
+        w <- w[!isNA]
+    }
     n <- length(w)
     xbarw <- sum(w*x) / sum(w)
     varw <- n / (n-1) * sum(w * (x-xbarw)^2) / sum(w)
